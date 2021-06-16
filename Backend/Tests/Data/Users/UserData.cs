47d5e57ae2bdfc2b3ae0964a549660e990079231
Keith Roland Lepik <roland.lepik@agileworks.eu>
2021-06-16 17:32:11 +0300

    Add postman tests

---
+++ Backend/Tests/Data/Users/UserData.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using Data.Common;
+using Data.Feedbacks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Data.Users
+{
+    [TestClass]
+    public class UserDataTests : BaseClassTests<UserData, UniqueEntityData>
+    {
+        private class TestClass : UserData { }
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+            Object = new TestClass();
+            Fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void FirstName_should_be_gettable_and_settable()
+        {
+            var randomValue = Fixture.Create<string>();
+
+            // Act
+            var initialValue = Object.FirstName;
+            Object.FirstName = randomValue;
+
+            // Assert
+            Assert.AreNotEqual(initialValue, randomValue);
+            Assert.AreEqual(Object.FirstName, randomValue);
+
+        }
+
+        [TestMethod]
+        public void LastName_should_be_gettable_and_settable()
+        {
+            var randomValue = Fixture.Create<string>();
+
+            // Act
+            var initialValue = Object.LastName;
+            Object.LastName = randomValue;
+
+            // Assert
+            Assert.AreNotEqual(initialValue, randomValue);
+            Assert.AreEqual(Object.LastName, randomValue);
+        }
+
+        [TestMethod]
+        public void UserName_should_be_gettable_and_settable()
+        {
+            var randomValue = Fixture.Create<string>();
+
+            // Act
+            var initialValue = Object.Username;
+            Object.Username = randomValue;
+
+            // Assert
+            Assert.AreNotEqual(initialValue, randomValue);
+            Assert.AreEqual(Object.Username, randomValue);
+        }
+    }
+}
--- Backend/Tests/Facade/Users/UsersMapperTests.cs
+++ Backend/Tests/Facade/Users/UsersMapperTests.cs
@@ -1,4 +1,6 @@
 ﻿using AutoFixture;
+using Data.Feedbacks;
+using Domain.Users;
 using Facade.Users;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -19,12 +21,26 @@ namespace Tests.Facade.Users
             var userRequest = Fixture.Create<UserRequest>();
 
             // Act
-            var user = UserMapper.MapRequestToDomain(userRequest); 
+            var user = UserMapper.MapRequestToDomain(userRequest);
 
             // Assert
-            Assert.AreEqual(userRequest.Username,user.Data.Username);
+            Assert.AreEqual(userRequest.Username, user.Data.Username);
             Assert.AreEqual(userRequest.LastName, user.Data.LastName);
             Assert.AreEqual(userRequest.FirstName, user.Data.FirstName);
+        }
+
+        [TestMethod]
+        public void MapDomainToModel_should_map_domain_to_model()
+        {
+            var userData = Fixture.Create<UserData>();
+
+            // Act
+            var model = UserMapper.MapDomainToModel(new User(userData), string.Empty);
+
+            // Assert
+            Assert.AreEqual(userData.Username, model.Username);
+            Assert.AreEqual(userData.LastName, model.LastName);
+            Assert.AreEqual(userData.FirstName, model.FirstName);
         }
     }
 }
